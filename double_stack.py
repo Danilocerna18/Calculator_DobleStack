869208b5accf3b8051ecfa923a06f274854b4a99
Mishelle Molina <mishellemolinaarana@gmail.com>
2026-05-14 11:56:45 -0600

    Created push for undo y redo

--- double_stack.py
+++ double_stack.py
@@ -2,3 +2,11 @@ class DoubleStack: #Clase de Double Stack
     def __init__(self): #Constructor de la clase 
         self.undo_stack = [] #Stack que almacena los estados anteriores 
         self.redo_stack = [] #Stack que almacena los estados deshechos
+        
+    def push_undo(self, value): #Método para agregar un valor al undo stack
+        self.undo_stack.append(value) #agrega el valor al final de la lista 
+
+    def push_redo(self,value): #Método para agregar un valor al redo stack
+        self.redo_stack.append(value) #Agrega el valor al final de la lista
+
+    
